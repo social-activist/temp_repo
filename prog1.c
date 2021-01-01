76afaf077017c05f3e1c6432e42b0bb052c96902
Mohammed Taqi Ahmed <mail2taqi@protonmail.com>
2021-01-01 21:37:00 +0530

    performing substraction in main file

--- prog1.c
+++ prog1.c
@@ -6,13 +6,15 @@
 #include "mymath.h"
 
 int main(void){
-	int first,second,result1;
+	int first,second,result1,result2;
 	printf("\n Enter first Number=\t");
 	scanf("%d",&first);
 	printf("\n Enter Second Number=\t");
 	scanf("%d",&second);
 	result1=myadd(first,second);
+	result2=mysub(first,second);
 	printf("\n%d	+	%d=%d\n",first,second,result1);
+	printf("\n%d	-	%d=%d\n",first,second,result2);
 	//return
 	return(EXIT_SUCCESS);
 }//end of main
